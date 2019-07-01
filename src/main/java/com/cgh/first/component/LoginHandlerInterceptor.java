0a5ffc313062009d36feb9ecd58bea71080d5e3e
cgh0315 <793533544@qq.com>
2019-07-01 22:28:19 +0800

    1.1.1

--- src/main/java/com/cgh/first/component/LoginHandlerInterceptor.java
+++ src/main/java/com/cgh/first/component/LoginHandlerInterceptor.java
@@ -7,18 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LoginHandlerInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Object username = request.getSession().getAttribute("username");
-        if (username == null){
-            request.setAttribute("message","error username or passwork");
-            request.getRequestDispatcher("index.html").forward(request,response);
-            return false;
-        }else {
-            return true;
-        }
-
-    }
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        Object username = request.getSession().getAttribute("username");
+//        if (username == null){
+//            request.setAttribute("message","error username or passwork");
+//            request.getRequestDispatcher("index.html").forward(request,response);
+//            return false;
+//        }else {
+//            return true;
+//        }
+//
+//    }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
--- src/main/java/com/cgh/first/controller/employController.java
+++ src/main/java/com/cgh/first/controller/employController.java
@@ -1,25 +1,65 @@
 package com.cgh.first.controller;
 
+import com.cgh.first.dao.DepartmentDao;
 import com.cgh.first.dao.EmployeeDao;
+import com.cgh.first.entities.Department;
 import com.cgh.first.entities.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 import java.util.Collection;
 
 @Controller
-public class employController {
+public class EmployController {
 
-    @Autowired
-    private EmployeeDao employeeDao;
+    @Resource
+    EmployeeDao employeeDao;
+
+    @Resource
+    DepartmentDao departmentDao;
 
     @GetMapping(value = "/emps")
-    public String getAllEmps(Model model){
-        Collection<Employee> all = employeeDao.getAll();
-        model.addAttribute("emps",all);
-        return "emp/list";
+    public String listEmploys(Model model){
+        Collection<Employee> employees = employeeDao.getAll();
+        model.addAttribute("emps",employees);
+        return "employ/list";
     }
 
+    @GetMapping(value = "/emp")
+    public String toAddPage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "employ/add";
+    }
+
+    @PostMapping(value = "/emp")
+    public String addEmp(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @GetMapping(value = "/emp/{id}")
+    public String toModifyEmp(@PathVariable("id") Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "employ/add";
+    }
+
+    @PutMapping(value = "/emp")
+    public String modifyEmp(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @DeleteMapping(value = "/emp/{id}")
+    public String deleteEmp(@PathVariable("id") Integer id,Model model){
+        employeeDao.delete(id);
+        return "redirect:/emps";
+    }
 }
